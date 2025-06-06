e5638a6401f2f06b60fbb5fa3875d21c13af4b56
Deepak Tyagi <deepak@Deepaks-MacBook-Air.local>
2025-06-06 12:23:09 +0530

    Added testcases for the weather app

---
+++ cypress.config.js
@@ -0,0 +1,8 @@
+const { defineConfig } = require("cypress");
+
+module.exports = defineConfig({
+   e2e: {
+        baseUrl: 'http://weatherapp-alb-1481460790.us-east-1.elb.amazonaws.com/',
+        specPattern: 'cypress/e2e/**/*.{js,jsx,ts,tsx}'
+    }
+});
---
+++ cypress/e2e/weather.cy.js
@@ -0,0 +1,38 @@
+describe('Weather App UI', () => {
+  before(() => {
+    cy.fixture('weather_data.json').then((data) => {
+      globalThis.data = data;
+  });
+  } )
+    beforeEach(() => {
+      cy.visit('/'); 
+    });
+  
+    it('Validates the page header', () => {
+      cy.get('h1').should('contain', data.pageHeader);
+    })
+    it('Loads default city (New York)', () => {
+      cy.get('#weatherCard').should('be.visible');
+      cy.get('#cityName').should('contain', data.defaultCity);
+    });
+  
+    it('Fetches weather for valid city', () => {
+      cy.get('#cityInput').clear().type(data.validCity);
+      cy.get('#searchBtn').click();
+      cy.get('#weatherCard').should('be.visible');
+      cy.get('#cityName').should('contain', data.validCity);
+    });
+  
+    it('Shows error for invalid city', () => {
+      cy.get('#cityInput').clear().type(data.invalidCity);
+      cy.get('#searchBtn').click();
+      cy.get('#errorContainer').should('be.visible').and('contain', data.errorMessage);
+    });
+  
+    it('Shows error for empty city input', () => {
+      cy.get('#cityInput').clear();
+      cy.get('#searchBtn').click();
+      cy.get('#errorContainer').should('be.visible');
+    });
+  });
+  
