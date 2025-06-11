b041c0f22d786a14612557ba04b5594b4f1167c5
Deepak Tyagi <deepak@Deepaks-MacBook-Air.local>
2025-06-11 19:14:01 +0530

    Removed the baseUrl to use localhost

--- cypress.config.js
+++ cypress.config.js
@@ -2,7 +2,7 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
    e2e: {
-        baseUrl: 'http://weatherapp-alb-1481460790.us-east-1.elb.amazonaws.com/',
+        baseUrl: 'http://localhost:3000/',
         specPattern: 'cypress/e2e/**/*.{js,jsx,ts,tsx}'
     },
     reporter: 'junit',
